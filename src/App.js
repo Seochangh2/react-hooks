660f35a476c1d00533f3a4cce2852edca7efa929
Seochangh2 <nomm89@naver.com>
2022-09-14 18:10:27 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import "./App.css";
-import useInput from "./UseInput";
-const App = () => {
-  const maxLen = (value) => value.length < 10;
-  const name = useInput("Mr.", maxLen);
+import useInput from "./Components/UseInput";
+import useTabs from "./Components/UseTabs";
 
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
+const App = () => {
+  const maxLen = (value) => value.length < 20;
+  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
-    <div>
+    <div className="App">
       <h1>Hello 창창</h1>
       <input placeholder="Name" {...name}></input>
+      <div>
+        {content.map((section, index) => (
+          <button
+            onClick={() => {
+              changeItem(index);
+            }}
+          >
+            {section.tab}
+          </button>
+        ))}
+      </div>
+      <div> {currentItem.content}</div>
     </div>
   );
 };
